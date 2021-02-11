dd823d10c822329935b9656614eae2dda3ea4988
ks-kon <kskon@bk.ru>
2021-02-11 20:39:43 +0300

    stage commit

--- n11111111/Source.cpp
+++ n11111111/Source.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+void change() {}
 
 int main()
 {
-
+	std::cout << "change A" << std::endl;
+	std::cout << "change B" << std::endl;
+	std::cout << "change C" << std::endl;
 	return EXIT_SUCCESS;
 }
